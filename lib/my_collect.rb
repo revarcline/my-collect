39d36d2349d9bdd41447022315a3094e5b9e71c2
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-08 13:27:44 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(list)
+  unless list == []
+    i = 0
+    collection = []
+    while i < list.length
+      collection << yield(list[i])
+      i += 1
+    end
+    collection
+  end
+end
